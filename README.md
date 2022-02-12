6cb7eb5b82717fc715df5892491d8948f021c164
ShardedFilms <98332856+ShardedFilms@users.noreply.github.com>
2022-02-12 23:05:48 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# GG
+A testing  js sharded content and it will added in future of this world.
---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+Vars.enebleConsole=true;
+Blocks.powerSource.powerProduction=655360;
+require('Planet');
+require('lib');
+require('sectorSize');
